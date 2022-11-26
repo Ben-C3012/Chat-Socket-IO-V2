fb2d9c43a5cf0fb7929ff31a69294288cc6f194a
unknown <ben.iisr.2004@gmail.com>
2022-11-26 20:35:54 +0200

    time fix

--- utils/messages.js
+++ utils/messages.js
@@ -1,12 +1,16 @@
-const moment = require('moment')
+const dayjs = require('dayjs')
+
+
 
 function formatMessage(username, text) {
     return {
         username,
         text,
-        time: moment().format('h:mm a')
+        // time: moment().format('h:mm a')
+        time: dayjs().hour() + ':' + dayjs().minute()
     }
 }
+
 
 
 module.exports = formatMessage
