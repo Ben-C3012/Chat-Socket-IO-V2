bd42131d18ab82aee2dce81c4135fa23b2eaa5f4
unknown <ben.iisr.2004@gmail.com>
2023-01-08 10:55:23 +0200

    time fix

--- utils/messages.js
+++ utils/messages.js
@@ -1,10 +1,9 @@
-const moment = require('moment')
 
 function formatMessage(username, text) {
     return {
         username,
         text,
-        time: `${moment().get('hour')}:${moment().get('minute')}`
+        time: `${new Date().getHours()}:${new Date().getMinutes()}`
     }
 }
 
