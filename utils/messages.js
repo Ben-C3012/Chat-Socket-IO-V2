cb51db55469b958bfc4209356f82de8ee57494ca
unknown <ben.iisr.2004@gmail.com>
2023-01-15 10:03:08 +0200

    time fix

--- utils/messages.js
+++ utils/messages.js
@@ -1,11 +1,13 @@
 const moment = require('moment')
+const axios = require('axios')
 
 function formatMessage(username, text) {
     return {
         username,
         text,
-        time: moment().format('HH:mm')
+        time: moment().format('').slice(11, 16)  
     }
 }
+
 
 module.exports = formatMessage
